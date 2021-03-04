d73fc83bbd821aee4adac4660f26c26b96cbb6f4
Thomas Lucas <tom@thomaslucas.co.uk>
2021-03-04 11:58:11 +0000

    Tidy util imports

--- src/tests/Util.hx
+++ src/tests/Util.hx
@@ -1,7 +1,9 @@
 package tests;
 
+import graphql.GraphQLField;
+
 class Util {
-    public static function getFieldDefinitionByName(fieldList : Array<graphql.GraphQLField>, name:String ) {
+    public static function getFieldDefinitionByName(fieldList : Array<GraphQLField>, name:String ) {
         for(field in fieldList) {
             if(field.name == name) {
                 return field;
