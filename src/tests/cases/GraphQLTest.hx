d2bd919dab5683b115a69e3dfe0dab58572abc5c
Thomas Lucas <tom@thomaslucas.co.uk>
2021-03-10 10:28:40 +0000

    Add more tests to graphql querying

--- src/tests/cases/GraphQLTest.hx
+++ src/tests/cases/GraphQLTest.hx
@@ -1,5 +1,6 @@
 package tests.cases;
 
+import php.Syntax;
 import php.NativeArray;
 import graphql.externs.GraphQL;
 import graphql.externs.Schema;
@@ -18,14 +19,17 @@ class GraphQLInstanceTest extends GraphQLObject {
     /**
         Object field comment
     **/
-    public var object_field:OtherObject;
+    public var object_field:OtherObject = new OtherObject();
+
+    public var nested_int : Array<Array<Int>> = [[1],[5,6]];
 }
 
 class OtherObject extends GraphQLObject {
+    public function new(){}
     /**
         String field comment
     **/
-    public var string_field:String = 'This is an instance value';
+    public var string_field:String = 'This is a value on the sub-object';
 }
 
 class GraphQLTest extends utest.Test {
@@ -43,16 +47,38 @@ class GraphQLTest extends utest.Test {
                 '{
                     string_field
                     renamed:string_field
+                    nested_int
+                    object_field {
+                        string_field
+                    }
                 }',
                 new GraphQLInstanceTest()
             );
             result.errors == [].toPhpArray();
             Assert.notNull(result.data);
-            var expected_result : Map<String, Dynamic> = [
-                'string_field' => 'This is an instance value',
-                'renamed' => 'This is an instance value'
-            ];
-            result.data == expected_result.associativeArrayOfHash();
+            if(result.data != null) {
+                var data : Map<String, Dynamic> = result.data.hashOfAssociativeArray();
+
+                var keys = [for(k in data.keys()) k];
+                var expected_keys = ['string_field', 'renamed', 'nested_int', 'object_field'];
+                Assert.same(keys,expected_keys,null,'Key list mismatch. Got: $keys, expected: $expected_keys');
+
+                data['string_field'] ==  'This is an instance value';
+                data['renamed'] == result.data['string_field'];
+                data['nested_int'] == [
+                    [1].toPhpArray(),
+                    [5,6].toPhpArray()
+                ].toPhpArray();
+                Assert.notNull(data['object_field'], 'object_field is null');
+                if(data['object_field'] != null) {
+                    var subobject = Lib.hashOfAssociativeArray( data['object_field'] );
+                    // Use same() assertion here instead of equality since haxe arrays are underlying objects which will be different instances
+                    var keys = [for(k in subobject.keys()) k];
+                    var expected_keys = ['string_field'];
+                    Assert.same(keys,expected_keys,null,'Key list mismatch. Got: $keys, expected: $expected_keys');
+                    subobject['string_field'] == 'This is a value on the sub-object';
+                }
+            }
         }
 }
 
