dae6aa9f30b4f2d076168622190f0ef518d8a4b2
Thomas Lucas <tom@thomaslucas.co.uk>
2021-03-11 15:03:43 +0000

    Add unit tests for function resolvers

--- src/tests/cases/GraphQLTest.hx
+++ src/tests/cases/GraphQLTest.hx
@@ -24,7 +24,20 @@ class GraphQLInstanceTest extends GraphQLObject {
 	public var object_field:OtherObject = new OtherObject();
 
     public var nested_int:Array<Array<Int>> = [[1], [5, 6]];
-    public var float: Float = 7.2;
+	public var float: Float = 7.2;
+	
+
+    public function greet(name:String) : String {
+        return 'Hello, $name';
+	}
+	
+	public function person(name:String) : Person {
+        return new Person(name);
+    }
+	
+	public function divide(x:Int, y:Int) : Float {
+        return x / y;
+    }
 }
 
 class OtherObject extends GraphQLObject {
@@ -34,6 +47,20 @@ class OtherObject extends GraphQLObject {
 		String field comment
 	**/
 	public var string_field:String = 'This is a value on the sub-object';
+}
+
+class Person extends GraphQLObject {
+	public function new(name:String) {
+		_name = name;
+	}
+
+	private var _name : String;
+	/**
+		String field comment
+	**/
+	public function name():String {
+		return 'This person has the name: $_name';
+	}
 }
 
 class GraphQLTest extends utest.Test {
@@ -60,7 +87,12 @@ class GraphQLTest extends utest.Test {
                     object_field {
                         string_field
                     }
-                    float
+					float
+					greet(name:"Unit tests")
+					person(name:"Herbert") {
+						name
+					}
+					divide(x: 7, y: 2)
                 }', new GraphQLInstanceTest());
 		result.errors == [].toPhpArray();
 		Assert.notNull(result.data);
@@ -68,13 +100,16 @@ class GraphQLTest extends utest.Test {
 			var data:Map<String, Dynamic> = result.data.hashOfAssociativeArray();
 
 			var keys = [for (k in data.keys()) k];
-			var expected_keys = ['string_field', 'renamed', 'nested_int', 'object_field', 'float'];
+			var expected_keys = ['string_field', 'renamed', 'nested_int', 'object_field', 'float', 'greet', 'person', 'divide'];
 			Assert.same(keys, expected_keys, null, 'Key list mismatch. Got: $keys, expected: $expected_keys');
 
 			data['string_field'] == 'This is an instance value';
 			data['renamed'] == result.data['string_field'];
             data['nested_int'] == [[1].toPhpArray(), [5, 6].toPhpArray()].toPhpArray();
-            data['float'] == 7.2;
+			data['float'] == 7.2;
+			data['greet'] == 'Hello, Unit tests';
+			data['divide'] == 3.5;
+
 			Assert.notNull(data['object_field'], 'object_field is null');
 			if (data['object_field'] != null) {
 				var subobject = Lib.hashOfAssociativeArray(data['object_field']);
@@ -83,6 +118,16 @@ class GraphQLTest extends utest.Test {
 				var expected_keys = ['string_field'];
 				Assert.same(keys, expected_keys, null, 'Key list mismatch. Got: $keys, expected: $expected_keys');
 				subobject['string_field'] == 'This is a value on the sub-object';
+			}
+			
+			Assert.notNull(data['person'], 'person is null');
+			if (data['person'] != null) {
+				var subobject = Lib.hashOfAssociativeArray(data['person']);
+				// Use same() assertion here instead of equality since haxe arrays are underlying objects which will be different instances
+				var keys = [for (k in subobject.keys()) k];
+				var expected_keys = ['name'];
+				Assert.same(keys, expected_keys, null, 'Key list mismatch. Got: $keys, expected: $expected_keys');
+				subobject['name'] == 'This person has the name: Herbert';
 			}
 		}
 	}
