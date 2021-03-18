4b260e7856bb95fe3ad5d6d40b7b56c496ed57b5
Thomas Lucas <tom@thomaslucas.co.uk>
2021-03-18 10:36:05 +0000

    Test dynamic returning of Mutation object type

--- src/tests/cases/MutationTest.hx
+++ src/tests/cases/MutationTest.hx
@@ -1,5 +1,6 @@
 package tests.cases;
 
+import php.NativeAssocArray;
 import graphql.GraphQLError;
 import graphql.GraphQLServer;
 import graphql.GraphQLObject;
@@ -63,6 +64,56 @@ class MutationTest extends utest.Test {
         var error_message : String = @:privateAccess error.getMessage();
         error_message.startsWith('Cannot query field "queryOnlyField" on type "MutationTestObjectMutation"') == true;
     }
+
+    function specDynamicMutationReturn() {
+        // Query
+        var response = server.executeQuery('{
+            dynamicMutationReturnTest {
+                queryOnlyField
+            }
+        }');
+        var data = (response.data : NativeAssocArray<NativeAssocArray<Dynamic>>);
+        data['dynamicMutationReturnTest']['queryOnlyField'] == "Query Only";
+
+
+        var response = server.executeQuery('{
+            dynamicMutationReturnTest {
+                mutationOnlyField
+            }
+        }');
+        Assert.isNull(response.data);
+        Assert.notNull(response.errors);
+
+        var errors = response.errors.toHaxeArray();
+        errors.length == 1;
+        var error = errors[0];
+        var error_message : String = @:privateAccess error.getMessage();
+        error_message.startsWith('Cannot query field "mutationOnlyField" on type "DynamicMutationReturnTestObject"') == true;
+
+        // Mutation
+        var response = server.executeQuery('mutation {
+            dynamicMutationReturnTest {
+                mutationOnlyField
+            }
+        }');
+        var data = (response.data : NativeAssocArray<NativeAssocArray<Dynamic>>);
+        data['dynamicMutationReturnTest']['mutationOnlyField'] == "Mutation Only";
+
+
+        var response = server.executeQuery('mutation {
+            dynamicMutationReturnTest {
+                queryOnlyField
+            }
+        }');
+        Assert.isNull(response.data);
+        Assert.notNull(response.errors);
+
+        var errors = response.errors.toHaxeArray();
+        errors.length == 1;
+        var error = errors[0];
+        var error_message : String = @:privateAccess error.getMessage();
+        error_message.startsWith('Cannot query field "queryOnlyField" on type "DynamicMutationReturnTestObjectMutation"') == true;
+    }
 }
 
 
@@ -76,4 +127,12 @@ class MutationTestObject extends GraphQLObject {
     @:mutation public var mutationOnlyField : String = "Mutation Only";
 
     @:query @:mutation public var bothField : String = "Will appear on both";
+    @:query @:mutation public var dynamicMutationReturnTest : DynamicMutationReturnTestObject = new DynamicMutationReturnTestObject();
+
+}
+
+class DynamicMutationReturnTestObject extends GraphQLObject {
+    public function new() {}
+    public var queryOnlyField : String = "Query Only";
+    @:mutation public var mutationOnlyField : String = "Mutation Only";
 }
