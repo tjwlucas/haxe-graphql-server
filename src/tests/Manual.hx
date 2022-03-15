3abf596bbfac2e5a919748098a95004e30008c90
Thomas Lucas <tom@thomaslucas.co.uk>
2022-03-15 10:35:36 +0000

    Add php 8.1 workaround for compiled output

--- src/tests/Manual.hx
+++ src/tests/Manual.hx
@@ -25,6 +25,8 @@ class Manual {
 
 	static function __init__() {
 		php.Global.require_once('vendor/autoload.php');
+		// PHP 8.1 compatibility workaround https://github.com/HaxeFoundation/haxe/issues/10502
+		untyped if (version_compare(PHP_VERSION, "8.1.0", ">=")) error_reporting(error_reporting() & ~E_DEPRECATED);
 	}
 }
 
--- src/tests/TestLoader.hx
+++ src/tests/TestLoader.hx
@@ -12,6 +12,8 @@ class TestLoader {
 
 	static function __init__() {
 		php.Global.require_once('vendor/autoload.php');
+		// PHP 8.1 compatibility workaround https://github.com/HaxeFoundation/haxe/issues/10502
+		untyped if (version_compare(PHP_VERSION, "8.1.0", ">=")) error_reporting(error_reporting() & ~E_DEPRECATED);
 	}
 }
 
